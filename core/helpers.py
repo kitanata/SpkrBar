b6c5316809317c63f654cf9ee314ef8e76bc990e
Raymond Chandler III <raymondchandleriii@gmail.com>
2013-06-05 14:23:23 -0400

    .

--- core/helpers.py
+++ core/helpers.py
@@ -1,12 +1,12 @@
 import uuid
 import os
-from config.settings import STATIC_ROOT
+from config.settings import MEDIA_ROOT
 
 def save_photo_with_uuid(photo):
     photo_ext = photo.name.split('.')[-1]
     photo_root_name = str(uuid.uuid4()) + '.' + photo_ext
     photo_name = os.path.join('photo', photo_root_name)
-    photo_storage = os.path.join(STATIC_ROOT, 'photo', photo_root_name)
+    photo_storage = os.path.join(MEDIA_ROOT, 'photo', photo_root_name)
 
     with open(photo_storage, 'wb+') as destination:
         for chunk in photo.chunks():
