ffcde2de32dc38c9d18a2ef703122e835650acd8
Raymond Chandler III <raymondchandleriii@gmail.com>
2013-07-09 12:17:03 -0400

    Finished the invite emailer code. Mailer still needs to be setup server side.

--- core/views/profile_invite.py
+++ core/views/profile_invite.py
@@ -1,25 +1,58 @@
 from string import Template as StringTemplate
+from email.utils import parseaddr
 from django.shortcuts import redirect
+from django.core.mail import send_mass_mail
 
 from core.helpers import template
+
+email_template = ""
+
+with open('mail/invite.html') as email_file:
+    email_template = StringTemplate(''.join(email_file.readlines()))
 
 @template('auth/invite.haml')
 def profile_invite(request):
     if request.method == "POST":
+        contacts = request.POST['contacts']
+        message = request.POST['message']
+
+        contacts = contacts.split(',')
+        contacts = [parseaddr(x) for x in contacts]
+
+        whole_message = StringTemplate(
+            """Hi $name,
+            <p>$message</p>
+            
+            <p>It's free for speakers and attendees!</p>
+
+            <a class="btn" href="http://www.spkrbar.com/register">Signup Here</a>
+
+            <p>I hope to see you on there soon.</p>
+
+            <blockquote>$username</blockquote>""")
+
+        messages = []
+        for contact in contacts:
+            if contact[0]:
+                mes = whole_message.substitute(
+                        name=contact[0], message=message,
+                        username=request.user.get_full_name())
+            else:
+                mes = whole_message.substitute(
+                        name="Friend", message=message,
+                        username=request.user.get_full_name())
+
+            messages.append(("You've been invited to Spkrbar.com!",
+                        email_template.substitute(message=mes),
+                        'invite@spkrbar.com', [contact[0]]))
+
+        send_mass_mail(messages, fail_silently=False)
         return redirect('/profile/invite/thanks')
     else:
-        template_string = \
-        """    I just joined spkrbar.com. Spkrbar is a cool new website for people who attend, plan, and speak at conferences. $other_part
+        template_string =  "I just joined spkrbar.com. Spkrbar is a cool new "\
+            "website for people who attend, plan, and speak at " \
+            "conferences. $other_part"
             
-            It's free for speakers and attendees!
-
-            You can join me at Spkrbar.com by signing up at http://www.spkrbar.com/register
-
-            I hope to see you on there soon.
-
-            - $user_name
-        """
-
         speaker_message = "Since I'm a speaker I can upload all the information " \
         "about my talks in one place and if you signup as an attendee you can " \
         "rate them and find other talks like mine you'd find interesting or insightful."
@@ -36,19 +69,16 @@ def profile_invite(request):
 
         invite_template = StringTemplate(template_string)
 
-        message = """Hi Friend,
-
-        """
         if request.user.is_speaker():
-            message += invite_template.substitute(
+            message = invite_template.substitute(
                     other_part=speaker_message,
                     user_name=request.user.get_full_name())
         elif request.user.is_attendee():
-            message += invite_template.substitute(
+            message = invite_template.substitute(
                     other_part=attendee_message,
                     user_name=request.user.get_full_name())
         elif request.user.is_event_planner():
-            message += invite_template.substitute(
+            message = invite_template.substitute(
                     other_part=planner_message,
                     user_name=request.user.get_full_name())
 
