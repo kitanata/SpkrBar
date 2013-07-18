c950a88f0e1a17ba2e2851a2b19556b2b43544d9
Raymond Chandler III <raymondchandleriii@gmail.com>
2013-07-18 17:29:36 -0400

    Basic Admin Stuff.

--- core/admin.py
+++ core/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from core.models import SpeakerProfile, EventProfile, ProfileTag, SpeakerLink
+from core.models import *
 
+admin.site.register(SpkrbarUser, admin.ModelAdmin)
 admin.site.register(SpeakerProfile, admin.ModelAdmin)
 admin.site.register(EventProfile, admin.ModelAdmin)
 admin.site.register(ProfileTag, admin.ModelAdmin)
--- talks/admin.py
+++ talks/admin.py
@@ -14,7 +14,10 @@ class TalkCommentAdmin(admin.ModelAdmin):
     list_display = ['talk', 'commenter', 'comment', 'datetime']
 
 admin.site.register(Talk, TalkAdmin)
-admin.site.register(TalkTag, admin.ModelAdmin)
 admin.site.register(TalkComment, TalkCommentAdmin)
 admin.site.register(TalkSlideDeck, TalkSlideDeckAdmin)
 admin.site.register(TalkVideo, TalkVideoAdmin)
+
+admin.site.register(TalkTag, admin.ModelAdmin)
+admin.site.register(TalkPhoto, admin.ModelAdmin)
+admin.site.register(TalkRating, admin.ModelAdmin)
