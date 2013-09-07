353dbd3e3f5f972e5d226a3dfdc6366dc7c70869
Nick Depinet <depinetnick@gmail.com>
2013-09-07 16:40:38 -0400

    limit speakers in speaker list to those with actual talks

--- core/views/speaker_list.py
+++ core/views/speaker_list.py
@@ -5,13 +5,16 @@ from core.helpers import template
 
 from core.models import SpeakerProfile
 
+from talks.models import Talk
+
 @template('speaker_list.haml')
 def speaker_list(request):
     speakers = SpeakerProfile.objects.all().annotate(
             num_tags=Count('tags'),
             num_links=Count('user__links')).order_by(
-                    '-photo', '-about_me', '-num_tags', '-num_links')[:20]
-
+                    '-photo', '-about_me', '-num_tags', '-num_links')
+    talks_speakers = list(set(talk.speaker for talk in Talk.objects.all()))
+    speakers = [spkr for spkr in speakers if spkr in talks_speakers][:20]
     return {
             'speakers': speakers,
             'last': '/speakers' }
