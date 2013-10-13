ec5e7f1512bd261560f194e828b2dab72d9a705a
Raymond Chandler III <raymondchandleriii@gmail.com>
2013-10-13 14:30:09 -0400

    Updated the search to work with the new models.

--- core/views/event_list.py
+++ core/views/event_list.py
@@ -1,8 +1,6 @@
 from datetime import datetime, timedelta
-from itertools import groupby
-from django.db.models import Count
 
-from core.helpers import template
+from core.helpers import template, events_from_engagements
 from engagements.models import Engagement
 
 @template('event_list.haml')
@@ -11,27 +9,9 @@ def event_list(request):
     end_date = datetime.today() + timedelta(days=14)
 
     engagements = Engagement.objects.filter(
-        date__gte=start_date.date(), date__lt=end_date.date()
-        ).order_by('event_name')
+        date__gte=start_date.date(), date__lt=end_date.date())
 
-    events = list()
-    for name, engs in groupby(engagements, key=lambda x: x.event_name):
-        engs = list(engs)
-        earliest_eng = min(map(lambda x: datetime.combine(x.date, x.time), engs))
-        latest_eng = max(map(lambda x: datetime.combine(x.date, x.time), engs))
-        tags = list(reduce(lambda x, y : x | y, map(lambda z: z.talk.tags.all(), engs)))
-        tags = sorted([(tags.count(x), x.name) for x in set(tags)], key=lambda x: -x[0])
-        events.append({
-            'name': name,
-            'numtalks': len(engs),
-            'earliest_date': earliest_eng.date,
-            'earliest_time': earliest_eng.time,
-            'latest_date': latest_eng.date,
-            'latest_time': latest_eng.time,
-            'tags': tags,
-        })
-
-    events.sort(key=lambda x: -x['numtalks'])
+    events = events_from_engagements(engagements)
 
     return {
         'events': events
--- core/views/search.py
+++ core/views/search.py
@@ -1,11 +1,12 @@
 import random
+import operator
 from datetime import datetime
 from django.db.models import Q
 from django.shortcuts import redirect
 
-from core.helpers import template
+from core.helpers import template, events_from_engagements
 
-from talks.models import TalkTag
+from talks.models import Talk, TalkTag
 from engagements.models import Engagement
 from core.models import SpkrbarUser, UserTag
 
@@ -16,35 +17,37 @@ def search(request):
 
     query = request.GET['query']
 
-    if len(query) < 3:
-        return redirect('/')
+    query = query.split(' ')
 
-    talk_tags = TalkTag.objects.filter(name__icontains=query)
+    talk_tags = TalkTag.objects.filter(
+        reduce(operator.and_, (Q(name__icontains=x) for x in query)))
+    user_tags = UserTag.objects.filter(
+        reduce(operator.and_, (Q(name__icontains=x) for x in query)))
 
-    talks = Engagement.objects.filter(
-            Q(talk__published=True), 
-            Q(date__gte=datetime.today()),
-            Q(talk__name__icontains=query) | 
-            Q(event__owner__name__icontains=query) | 
-            Q(talk__tags__in=talk_tags))
+    talks = Talk.objects.filter(
+            Q(published=True), 
+            reduce(operator.and_, (Q(name__icontains=x) for x in query)) |
+            Q(tags__in=talk_tags)
+            ).distinct()
 
-    events = Event.objects.filter(
-            Q(owner__name__icontains=query) |
-            Q(name__icontains=query) |
-            Q(location__name__icontains=query))
+    events = Engagement.objects.filter(
+            reduce(operator.and_, (Q(event_name__icontains=x) for x in query)) |
+            reduce(operator.and_, (Q(location__name__icontains=x) for x in query))
+            ).distinct()
 
-    profile_tags = ProfileTag.objects.filter(name__icontains=query)
+    events = events_from_engagements(events)
 
     speakers = SpkrbarUser.objects.filter(
-            Q(username__icontains=query) |
-            Q(first_name__icontains=query) |
-            Q(last_name__icontains=query) |
-            Q(tags__in=profile_tags))
+            reduce(operator.and_, (Q(username__icontains=x) for x in query)) |
+            reduce(operator.and_, (Q(first_name__icontains=x) for x in query)) |
+            reduce(operator.and_, (Q(last_name__icontains=x) for x in query)) |
+            Q(tags__in=user_tags)
+            ).distinct()
 
     if talks.count() > 12:
         talks = random.sample(talks, 12)
 
-    if events.count() > 12:
+    if len(events) > 12:
         events = random.sample(events, 12)
 
     if speakers.count() > 12:
